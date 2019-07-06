095e290c0a2a0e5ea8267d4eb85dba8a43ac3d80
Dmitriy.Vtulkin <vtulich@mail.ru>
2019-07-06 15:21:53 +0300

    fix scripts

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-sudo apt update &&  \
-sudo apt install -y ruby-full ruby-bundler build-essential \
-ruby -v \
-bundler -v
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
